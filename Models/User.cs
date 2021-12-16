44fb1f21040f126e9f92a1b4919981c5d619dc32
flesarradu <flesarradu@gmail.com>
2021-12-16 02:54:31 +0200

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ProiectP3_BackendApp.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ProiectP3_BackendApp.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) 
+            : base(options) { 
+        }
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
